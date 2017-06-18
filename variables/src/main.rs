a1b48cfff5e6416912dc3e08eed69668b52ee731
Alex Polyakov <warr4uk@yandex.ru>
2017-06-18 12:51:20 +0300

    add to proceed comment

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
 }
+
+// https://doc.rust-lang.org/book/second-edition/ch03-02-data-types.html
